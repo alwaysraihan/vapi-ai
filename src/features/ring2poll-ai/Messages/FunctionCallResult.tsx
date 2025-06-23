02af4fd3cae0e7f2ffbd71f0ef3fad812c5a50b1
Raihan Sarkar <83104019+alwaysraihan@users.noreply.github.com>
2025-06-23 22:29:37 +0600

    fix: deployment

--- src/features/ring2poll-ai/Messages/FunctionCallResult.tsx
+++ src/features/ring2poll-ai/Messages/FunctionCallResult.tsx
@@ -12,7 +12,9 @@ export default function FunctionCallResult({
       className={`flex w-4/5 text-sm mb-4 justify-end text-[#1a0400] font-medium mx-auto`}
     >
       <div className={`p-3 rounded-xl bg-green-100 mx-auto`}>
-        <p className="leading-relaxed">{message.functionCallResult.result}</p>
+        <p className="leading-relaxed">
+          {message?.functionCallResult?.result as string}
+        </p>
       </div>
     </div>
   );
